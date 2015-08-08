6d20a763632d37c77204e3df9d9512755c0fe5f5
Keiji Yahata <yahata@nowhere.co.jp>
2015-08-09 02:03:31 +0900

    Initial Commit

---
+++ osxapp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  osxapp
+//
+//  Created by Keiji Yahata on 8/9/15.
+//  Copyright (c) 2015 Keiji Yahata. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ osxapp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  osxapp
+//
+//  Created by Keiji Yahata on 8/9/15.
+//  Copyright (c) 2015 Keiji Yahata. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
